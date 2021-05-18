17fc3d42d12614c716d8e8a4bd521ad0f5e663f6
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-05-18 17:50:09 +0200

    Implementata struttura con titolo, prezzo ed info serie selezionata

--- laravel-main/resources/views/pages/serie.blade.php
+++ laravel-main/resources/views/pages/serie.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <main>
-        <div class="blue-separator img-container">
+        <section class="blue-separator img-container">
 
             <div class="float-img">
                 <div class="type">
@@ -15,7 +15,25 @@
                     VIEW GALLERY
                 </div>
             </div>
-        </div>
+        </section>
+
+        <section class="description">
+            <div class="info">
+                <h2>
+                    {{strtoupper($thisSerie['title'])}}
+                </h2>
+
+                <div class="sell">
+                    <span>U.S. Price: {{$thisSerie['price']}}</span>
+                    <span>AVAIABLE</span>
+                    <span>Check Avaiability <i class="fas fa-caret-down"></i></span>
+                </div>
+
+                <p>
+                    {{$thisSerie['description']}}
+                </p>
+            </div>
+        </section>
 
     </main>
 @endsection
