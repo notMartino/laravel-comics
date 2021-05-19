90e70a0b55c4296c8ae0a44324c3904a50ec378d
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-05-19 15:39:26 +0200

    Esercizio completo: pag. serie selezionata completa

--- laravel-main/resources/views/pages/serie.blade.php
+++ laravel-main/resources/views/pages/serie.blade.php
@@ -61,21 +61,102 @@
 
         <section class="specs">
             <div>
-                <h2>Talent</h2>
-                <div class="row">
-                    <h5>
-                        Art by:
-                    </h5>
-                    <p>
-                        CIaoo
-                    </p>
-                </div>
+                <ul class="box rm">
+                    <li class="title">
+                        <h2>Talent</h2>
+                    </li>
+                    <li class="argument">
+                        <h5>
+                            Art by:
+                        </h5>
+                        <p>
+                            @foreach ($thisSerie['artists'] as $artist)
+                                @if (!$loop -> last)
+                                    {{$artist}},
+                                @else
+                                    {{$artist}}
+                                @endif
+                            @endforeach
+                            
+                        </p>
+                    </li>
+                    <li class="argument">
+                        <h5>
+                            Written by:
+                        </h5>
+                        <p>
+                            @foreach ($thisSerie['writers'] as $writer)
+                                @if (!$loop -> last)
+                                    {{$writer}},
+                                @else
+                                    {{$writer}}
+                                @endif
+                            @endforeach
+                            
+                        </p>
+                    </li>
+                </ul>
             </div>
 
             <div>
-                <h2>Specs</h2>
+                <ul class="box lm">
+                    <li class="title">
+                        <h2>Specs</h2>
+                    </li>
+                    <li class="argument">
+                        <h5>
+                            Series:
+                        </h5>
+                        <p>
+                            {{ strtoupper($thisSerie['series']) }}
+                        </p>
+                    </li>
+                    <li class="argument">
+                        <h5>
+                            U.S. Price:
+                        </h5>
+                        <p>
+                            {{ $thisSerie['price'] }}
+                        </p>
+                    </li>
+                    <li class="argument">
+                        <h5>
+                            On Sale Date:
+                        </h5>
+                        <p>
+                            {{ $thisSerie['sale_date'] }}
+                        </p>
+                    </li>
+                </ul>
+            </div>
+
+        </section>
+        
+        <section class="bottom-links">
+            <div class="box">
+                <h4>
+                    DIGITAL COMICS
+                </h4>
+                <img src="/storage/imgs/icons-1.png" alt="Icon 1">
+            </div>
+            <div class="box">
+                <h4>
+                    SHOP DC
+                </h4>
+                <img src="/storage/imgs/icons-4.png" alt="Icon 4">
+            </div>
+            <div class="box">
+                <h4>
+                    COMIC SHOP LOCATOR
+                </h4>
+                <img src="/storage/imgs/icons-3.png" alt="Icon 3">
+            </div>
+            <div class="box">
+                <h4>
+                    SUBSRIPTIONS
+                </h4>
+                <img src="/storage/imgs/icons-2.png" alt="Icon 2">
             </div>
         </section>
-
     </main>
 @endsection
